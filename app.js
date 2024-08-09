d2e03123b18c3f5c424eafdfa5e070ea331b0e5d
sindhurajesh123 <usindhu1988@gmail.com>
2024-08-09 12:45:24 +0530

    final

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 
-  function validateForm() {
-    var name = document.forms["myForm"]["fname"].value;
-    var email = document.forms["myForm"]["email"].value;
-    var password = document.forms["myForm"]["password"].value;
-    var message = document.forms["myForm"]["message"].value;
+function validateForm() {
+const name = document.forms["myForm"]["fname"].value;
+  const email = document.forms["myForm"]["email"].value;
+  const password = document.forms["myForm"]["password"].value;
+  const message = document.forms["myForm"]["message"].value;
 
-    if (name == "" || email == "" || password == "" || message == "") {
-      alert("All fields must be filled out");
-      return false;
-    }
+  if (name == "" || email == "" || password == "" || message == "") {
+    alert("All fields must be filled out");
+    return false;
   }
+}
+document.addEventListener("DOMContentLoaded", function() {
+  var detailsButton = document.querySelector('.your-class-name');
+  if (detailsButton) {
+      detailsButton.classList.add('singular');
+  } else {
+      console.error("Element not found: .your-class-name");
+  }
+});
